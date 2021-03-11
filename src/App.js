3755ffc1ef0bb2c270c8387cd064fc21de17312f
Jahnvi <jahnvi18patel@gmail.com>
2021-03-11 11:41:43 +0000

    initial setup and plan

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Home from './components/Home'
 
 function App() {
-  return <h1>Hello World</h1>
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Home />
+    </>
+  )
+
+
 }
 
 export default App
