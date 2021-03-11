904282d6aa16b9bb002369296b5e3d2fb711db72
Jahnvi <jahnvi18patel@gmail.com>
2021-03-11 15:25:51 +0000

    working random generation for both

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './components/Home'
+import axios from 'axios'
 
 function App() {
+  const [background, setBackground] = useState([])
+
+  useEffect(() => {
+    const getBackgroundData = async () => {
+      const response = await axios.get('https://source.unsplash.com/collection/1376954/landscape')
+      setBackground(response.config.url)
+    }
+    getBackgroundData()
+  }, [])
+
+
 
   return (
-    <>
+
+    <div className="background" style={{ backgroundImage: `url(${background})` }}>
       <h1>Hello World</h1>
       <Home />
-    </>
+    </div>
   )
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,34 +5,42 @@ import axios from 'axios'
 
 const Home = () => {
   //! Background
-  const [background, setBackground] = useState([])
-  const [quote, setQuotes] = useState('')
+  // const [background, setBackground] = useState([])
+  const [quotes, setQuotes] = useState('')
 
-  useEffect(() => {
-    const getBackgroundData = async () => {
-      const response = await axios.get('https://source.unsplash.com/collection/1376954/landscape')
-      setBackground(response)
-    }
-    getBackgroundData()
-  }, [])
+  // useEffect(() => {
+  //   const getBackgroundData = async () => {
+  //     const response = await axios.get('https://source.unsplash.com/collection/1376954/landscape')
+  //     setBackground(response)
+  //   }
+  //   getBackgroundData()
+  // }, [])
 
   //! Quotes
   useEffect(() => {
     const urlArray = ['https://quote-garden.herokuapp.com/api/v3/quotes?genre=motivational&limit=100', 'https://quote-garden.herokuapp.com/api/v3/quotes?genre=happiness&limit=100', 'https://quote-garden.herokuapp.com/api/v3/quotes?genre=time&limit=100']
+    const randomArray = urlArray[Math.floor(Math.random() * urlArray.length)]
     const getRandomQuote = async () => {
-      const { data } = await axios.get(urlArray[Math.floor(Math.random() * urlArray.length - 1)])
-      setQuotes(data.data[1])
-      console.log(data.data[Math.floor(Math.random() * data.data.length - 1)])
+      const { data } = await axios.get(randomArray)
+      const randomNumber = Math.floor(Math.random() * data.data.length)
+      setQuotes(data.data[randomNumber])
+      // console.log(data.data[Math.floor(Math.random() * data.data.length)])
     }
     getRandomQuote()
+
   }, [])
+
 
 
   return (
 
-    <div className="background" style={{ backgroundImage: `url(${background})` }}>
-      <p>{quote.quoteText}</p>
-      <p>{quote.quoteAuthor}</p>
+    <div className="background">
+      <div className="quote-text">
+        <p>{quotes.quoteText}</p>
+      </div>
+      <div className="quote-author">
+        <p>{quotes.quoteAuthor}</p>
+      </div>
     </div >
 
   )
